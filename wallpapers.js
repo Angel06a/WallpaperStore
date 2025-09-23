56cb02f60a545475de0db5e6067aab80e30f9784
Angel06 <ug25228@gmail.com>
2025-09-23 01:11:57 -0500

    Add files via upload

---
+++ wallpapers.js
@@ -0,0 +1,18 @@
+const wallpapersPC = [
+    "ChainsawMan.jpg",
+    "ModernXP-Day.png",
+    "ModernXP-Night.png",
+    "ModernXP-Sunset.png",
+    "Paisaje.jpg",
+    "ultramanVSgodzilla.jpg",
+    "WindowsBlue.jpg",
+    "WindowsPurple.jpg"
+    // Agrega aquí todos los nombres de archivo para PC
+];
+
+const wallpapersMovil = [
+    "demonslayer.png",
+    "hunterXhunter.png",
+    "ultramanVSgodzilla.png"
+    // Agrega aquí todos los nombres de archivo para Móvil
+];
